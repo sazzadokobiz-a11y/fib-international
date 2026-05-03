1c61116adf770ec8d83d8809f880f7bb1de495fb
sazzadokobiz-a11y <sazzad.okobiz@gmail.com>
2026-05-03 18:55:51 +0600

    [DONE]: Admin project setup and install shadcn

--- server/src/middleware/globalErrorHandler.ts
+++ server/src/middleware/globalErrorHandler.ts
@@ -1,17 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodError } from "zod";
 
 export function errorHandler (error: any, req: Request, res: Response, next: NextFunction){
     const statusCodeFromError = Number(error?.status);
     let statusCode = Number.isInteger(statusCodeFromError) ? statusCodeFromError : 500;
     let message = error.message || "Internal server error!!";
     let errorDetails = error;
-
-    if(error instanceof ZodError){
-        statusCode = 400;
-        message = "Validation failed";
-        errorDetails = error.issues;
-    }
 
     res.status(statusCode).json({
     success: false,

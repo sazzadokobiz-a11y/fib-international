4bd3008e62e291a916f07cd261926dac16194e74
sazzadokobiz-a11y <sazzad.okobiz@gmail.com>
2026-05-19 11:11:56 +0600

    [MODIFIED]: show dynamic data in export section

--- client/src/components/landing/ExportSection.tsx
+++ client/src/components/landing/ExportSection.tsx
@@ -3,8 +3,13 @@ import Container from '../shared/Container'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
+import { getExportProducts } from '@/services/product'
+import { ExportProduct } from '@/types/product'
 
-const ExportSection = () => {
+const ExportSection = async() => {
+    const {data} = await getExportProducts({limit: 4, sortBy: "createdAt", sortOrder: "desc"});
+    const products: ExportProduct[] = data.data;
+    
     return (
         <section className="py-32 bg-[#f5f0e6]">
             <Container>
@@ -12,7 +17,7 @@ const ExportSection = () => {
                     <div>
                         <span className="block text-secondary tracking-[0.05em] uppercase text-sm font-bold mb-4 font-label">Outbound Logistics</span>
                         <h2 className="text-primary font-headline font-semibold text-4xl md:text-[2.5rem] leading-[1.2] tracking-tight">
-                            Premium Export Curations
+                            Premium Export Products
                         </h2>
                     </div>
                     <Link
@@ -24,78 +29,25 @@ const ExportSection = () => {
                     </Link>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                    {/* Card 1 */}
-                    <div className="group bg-[#fcf9f4] rounded-xl overflow-hidden cursor-pointer transition-transform duration-500 hover:-translate-y-2">
-                        <div className="h-64 overflow-hidden">
-                            <Image
-                                alt="Lush green agricultural fields viewed"
-                                src="https://lh3.googleusercontent.com/aida-public/AB6AXuCHCo7OxekQmPm8cqQ4rOO4wNshO7ZLF5NlD7j-BZA-Zzsnd4IR8XPAGR2NT2lCFYlFFmezP8VUUoS-lEWBgCLsdVOd8aRsp6zmPcRxYBRcYDTrJkVH1WBT8logljVYJEgEmYFYqWgJiQ2neACD5vM-l6bQ_gbBjTjIpSD_fG8fWQhfHuYH7FCIJL_wZmLD70WLqR6Ez2dWP3wEl_2bYcoobSiXNL9_SVZrUDv6ssHCtalSU0DPrLU-zy3u3Ocl24MrARO1bAbyerc"
-                                height={800}
-                                width={800}
-                                className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-105"
-                            />
+                    {
+                        products?.map((prod: ExportProduct) => <div key={prod._id} className="group bg-[#fcf9f4] rounded-xl overflow-hidden cursor-pointer transition-transform duration-500 hover:-translate-y-2">
+                            <div className="h-64 overflow-hidden">
+                                <Image
+                                    alt="Lush green agricultural fields viewed"
+                                    src={prod.thumbnail}
+                                    height={800}
+                                    width={800}
+                                    className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-105"
+                                />
 
-                        </div>
-                        <div className="p-8">
-                            <h3 className="font-headline font-semibold text-xl text-primary mb-3">Agricultural Commodities</h3>
-                            <p className="font-body text-on-surface-variant text-sm leading-relaxed mb-6">Sourcing and distributing premium grains and produce across international borders.</p>
-                            <div className="h-1 w-8 bg-surface-variant group-hover:bg-secondary transition-colors duration-300"></div>
-                        </div>
-                    </div>
-                    {/* Card 2 */}
-                    <div className="group bg-[#fcf9f4] rounded-xl overflow-hidden cursor-pointer transition-transform duration-500 hover:-translate-y-2 mt-0 lg:mt-8">
-                        <div className="h-64 overflow-hidden">
-                            <Image
-                                alt="Close up detail of woven textile fabric"
-                                width={800}
-                                height={800}
-                                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
-                                data-alt="Close up detail of intricate woven textile fabric showing rich texture and vibrant interwoven threads"
-                                src="https://lh3.googleusercontent.com/aida-public/AB6AXuCNjlrKGgIub_SPrH8jEFnIFvz_MBQu0GmoimWqvE0y5pwzor_v_liUFG5uiK9v2BVJt01CtXEGZe7MufFiMLh0Zr76wOujrMPj-qlNrcgDqmfXsXWlG0mTY-m5zWiEg35JqhJPzmzcaMn-wz5gFVNflc76OSlpgW2oNXray2ywLk7NDJRRRsTK-Fkg_hZsrSd4FdWm0tHaARZ5dhO7MJnyzj0V8x-M7QtxwfA6hqZy2ZTmbBnhU_aamN69s6s2Usuofb7ESUDVUaY"
-                            />
-                        </div>
-                        <div className="p-8">
-                            <h3 className="font-headline font-semibold text-xl text-primary mb-3">Textiles &amp; Garments</h3>
-                            <p className="font-body text-on-surface-variant text-sm leading-relaxed mb-6">High-grade materials and finished apparel sourced from top-tier manufacturers.</p>
-                            <div className="h-1 w-8 bg-surface-variant group-hover:bg-secondary transition-colors duration-300"></div>
-                        </div>
-                    </div>
-                    {/* Card 3 */}
-                    <div className="group bg-[#fcf9f4] rounded-xl overflow-hidden cursor-pointer transition-transform duration-500 hover:-translate-y-2">
-                        <div className="h-64 overflow-hidden">
-                            <Image
-                                alt="Precision welding machinery in an industrial facility"
-                                width={800}
-                                height={800}
-                                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
-                                data-alt="Sparks flying from precision welding machinery in a dark modern industrial manufacturing facility"
-                                src="https://lh3.googleusercontent.com/aida-public/AB6AXuDiFn05pSltqU4FTBEMFIxUTnURUNvjjBUf0QINYEb4_pt-FEZqvF_JEaoZpkjSpgW5YoPg0HmNruJnwBGFJmR3FjaJALYR-WM3G3Emz25Pq3pxDZcC-NUul6HDZqenvv7vo0su6soUiD9d3vYKe3F0mrjJqUlylkRksbAGvm7Q72Wp1wGIGb-Cfxf10yFFg_CVg5LY9Mlj9WwXfd7wLJtbAyjGW9IkBhpD9fOVcStoFfChH02XW68Po3wJ2Sk26wh6Fw73CJB5Sns"
-                            />
-                        </div>
-                        <div className="p-8">
-                            <h3 className="font-headline font-semibold text-xl text-primary mb-3">Industrial Components</h3>
-                            <p className="font-body text-on-surface-variant text-sm leading-relaxed mb-6">Critical machinery parts ensuring seamless operational continuity globally.</p>
-                            <div className="h-1 w-8 bg-surface-variant group-hover:bg-secondary transition-colors duration-300"></div>
-                        </div>
-                    </div>
-                    {/* Card 4 */}
-                    <div className="group bg-[#fcf9f4] rounded-xl overflow-hidden cursor-pointer transition-transform duration-500 hover:-translate-y-2 mt-0 lg:mt-8">
-                        <div className="h-64 overflow-hidden">
-                            <Image
-                                alt="image of raw materials"
-                                width={800}
-                                height={800}
-                                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
-                                data-alt="Stack of polished raw metallic ingots catching dramatic studio lighting against a dark background"
-                                src="https://lh3.googleusercontent.com/aida-public/AB6AXuBvtz7xXWSJ8WitnaIOb-hbHMznX9HkLKMY4nO2FiWtNqdVpmX6aylG6AFSPXaMJ3uzUaaHK9NxyUsgmJFknbOddBXkfE_nFrawmNONRGs3oM6R4Q5ibfHOYY4pON6dOxey1aPVnFKmegDHEQHj-TSiobInuqc1tKGHeIfyJY0IcnpsYWWMMZnAx1FiXfAP8F8ch5owWWEL99FZGPrADdZ5wxynOqCFoqtTl3kgPV4Rei9_XktM5zI7iMrj87RwRMGuWZQ9WpSV7Yc"
-                            />
-                        </div>
-                        <div className="p-8">
-                            <h3 className="font-headline font-semibold text-xl text-primary mb-3">Raw Materials</h3>
-                            <p className="font-body text-on-surface-variant text-sm leading-relaxed mb-6">Ethically sourced foundational resources driving global infrastructure.</p>
-                            <div className="h-1 w-8 bg-surface-variant group-hover:bg-secondary transition-colors duration-300"></div>
-                        </div>
-                    </div>
+                            </div>
+                            <div className="p-8">
+                                <h3 className="font-headline font-semibold text-xl text-primary mb-3">{prod.name.length > 20 ? prod?.name?.slice(0, 20) + "..." : prod.name}</h3>
+                                <p className="font-body text-on-surface-variant text-sm leading-relaxed mb-6">{prod.description.length > 150 ? prod?.description?.slice(0, 150) + "..." : prod.description}</p>
+                                <div className="h-1 w-8 bg-surface-variant group-hover:bg-secondary transition-colors duration-300"></div>
+                            </div>
+                        </div>)
+                    }
                 </div>
 
             </Container>
